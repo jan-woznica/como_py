2e4d49295018312471b736f83d3f07f622114c48
jan-woznica <wozni@seznam.cz>
2023-04-02 10:51:00 +0200

    app.py

--- app.py
+++ app.py
@@ -15,21 +15,21 @@ add_selectbox = st.sidebar.selectbox('Menu', ('Čtverec', 'Obdélník', 'Kvádr'
 
 if add_selectbox == ('Čtverec'):
     číslo = int(input('Vepište 1. číslo 1.'))
-        obvod1 = 4*číslo
-        obsah1 = číslo*číslo
+    obvod1 = (číslo * 4)
+    obsah1 = (číslo*číslo)
     st.write(f'Obvod je {obvod1} a obsah je {obsah1}')
 elif add_selectbox == ('Obdélník'):
     číslo1 = input('Vepište stranu a')
     číslo5 = input('Vepište stranu b')
-        obvod2 = (číslo1+číslo5)*2
-        obsah2 = číslo1*číslo5
+    obvod2 = ((číslo1+číslo5)*2)
+    obsah2 = číslo1*číslo5
     st.write(f'Obvod je {obvod2} a obsah je {obsah2}')
 elif add_selectbox == ('Kvádr'):
     číslo2 = st.number_input('Vepište stranu a')
     číslo3 = st.number_input('Vepište stranu b')
     číslo4 = st.number_input('Vepište stranu c')
-        objem = číslo2*číslo3*číslo4
-        plocha = (2*((číslo2+číslo3)*(číslo3+číslo4)*(číslo2+číslo4))
+    objem = číslo2*číslo3*číslo4
+    plocha = (2*(číslo2+číslo3)*(číslo3+číslo4)*(číslo2+číslo4))
     st.write(f'Objem je {objem} a plocha je {plocha}')
 
 
